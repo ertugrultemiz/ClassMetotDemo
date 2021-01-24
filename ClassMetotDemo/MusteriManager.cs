90c325572dd5b228f44d30d0750dccacf3d03ece
ertuğrul temiz <ertugrultemizz@outlook.com>
2021-01-24 16:46:30 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+
+        
+	    {
+            Console.WriteLine("Musteri eklendi : " + musteri.Ad + " " + musteri.Soyad);
+        }       
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri silindi : " + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void MusteriListeleme(Musteri musteri)
+            
+        {
+                Console.WriteLine("Ad Soyad : " + musteri.Ad + " " + musteri.Soyad);
+                Console.WriteLine("Yas : " + musteri.Yas);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ertugrul";
+            musteri1.Soyad = "Temiz";
+            musteri1.Yas = 23;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Arzu";
+            musteri2.Soyad = "Karakomur";
+            musteri2.Yas = 20;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Ahmet";
+            musteri3.Soyad = "Yilmaz";
+            musteri3.Yas = 52;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.MusteriEkleme(musteri1);
+            musterimanager.MusteriEkleme(musteri2);
+            musterimanager.MusteriEkleme(musteri3);
+
+            musterimanager.MusteriSilme(musteri1);
+            musterimanager.MusteriSilme(musteri2);
+            musterimanager.MusteriSilme(musteri3);
+
+            musterimanager.MusteriListeleme(musteri1);
+            musterimanager.MusteriListeleme(musteri2);
+            musterimanager.MusteriListeleme(musteri3);
+        }
+    }
+}
